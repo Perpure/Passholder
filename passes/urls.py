1c0b3a1009de28c43b2420c723ace7fd5364d2f0
Paul Dyachek <dyachkep@gmail.com>
2017-12-27 11:41:12 -0500

    testing password recover

--- passes/urls.py
+++ passes/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'passes'
@@ -14,4 +14,8 @@ urlpatterns = [
     url(r'^delete_info/$', views.delete_info, name='delete_info'),
     url(r'^download_info/&', views.download_info, name='download_info'),
     url(r'^userpage/$', views.userpage, name='userpage'),
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
