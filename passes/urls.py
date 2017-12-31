cb312ea697a3f576f4ef4cafce841bbf4a747cba
Andrew Smirnov <smirnov@informatics.ru>
2017-12-31 13:09:37 +0300

    Fix ressword reset

--- passes/urls.py
+++ passes/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from django.urls import include
+
 from . import views
 
 app_name = 'passes'
@@ -14,8 +16,5 @@ urlpatterns = [
     url(r'^delete_info/$', views.delete_info, name='delete_info'),
     url(r'^download_info/&', views.download_info, name='download_info'),
     url(r'^userpage/$', views.userpage, name='userpage'),
-    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
-    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',auth_views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+    url('^', include('django.contrib.auth.urls')),
 ]
