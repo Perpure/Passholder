88a7c8100b992b72096221fd83c682367a60716b
Paul Dyachek <dyachkep@gmail.com>
2018-01-14 11:08:35 -0500

    added license and footer

--- passholder/urls.py
+++ passholder/urls.py
@@ -1,19 +1,21 @@
-"""testpolls URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+Copyright (C) 2017-2018 Pavel Dyachek GPL 3.0+
 
+This file is part of PassHolder.
+
+    PassHolder is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PassHolder is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with PassHolder.  If not, see <http://www.gnu.org/licenses/>.
+"""
 from django.conf.urls import include, url
 from django.contrib import admin
 
